de5a4130eb521388fe6d7128804bf8e44923f658
a <a>
2024-11-17 06:30:13 -0500

    Retooling WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::num;
-use std::{array, collections::{HashMap, HashSet}, hash::Hash, io};
+use std::{array, collections::{HashMap, HashSet, VecDeque}, hash::Hash, io};
 
 fn main() {
     let lines: Vec<String> = io::stdin().lines().map(|l| l.unwrap_or_default()).collect();
@@ -79,6 +79,194 @@ fn part1(lines: &Vec<String>) -> i32 {
     return reachable.into_iter().flatten().filter(|r| *r).count() as i32;
 }
 
+fn part2(lines: &Vec<String>) -> i64 {
+    let width = lines[0].len();
+    let height = lines.len();
+
+    // Validate input, convert to map
+    let tile_chars = lines.concat();
+    if tile_chars.len() != width * height {
+        println!("Invalid input, width = {}, height = {}, len = {}", width, height, width * height);
+        return 0;
+    }
+    let start_index = tile_chars.find('S').unwrap_or(0);
+
+    let mut tile_map = vec![vec![Tile::Rock; width]; height];
+    for (i, ch) in tile_chars.char_indices() {
+        let tile_x = i % height;
+        let tile_y = i / height;
+        tile_map[tile_y][tile_x] = match ch {
+            '#' => Tile::Rock,
+            _ => Tile::Garden
+        }
+    }
+
+    // Perform BFS to find distance from each tile to center
+    let mut distances = vec![vec![std::usize::MAX; width]; height];
+    let mut bfs_queue = VecDeque::new();
+
+    distances[height / 2][width / 2] = 0;
+    bfs_queue.push_back((height / 2, width / 2));
+    while !bfs_queue.is_empty() {
+        let (x, y) = bfs_queue.pop_front().unwrap();
+        let curr_tile_distance = distances[y][x];
+        if x + 1 < width && tile_map[y][x + 1] == Tile::Garden && distances[y][x + 1] == std::usize::MAX {
+            distances[y][x + 1] = curr_tile_distance + 1;
+            bfs_queue.push_back((x + 1, y));
+        }
+        if x >= 1 && tile_map[y][x - 1] == Tile::Garden && distances[y][x - 1] == std::usize::MAX {
+            distances[y][x - 1] = curr_tile_distance + 1;
+            bfs_queue.push_back((x - 1, y));
+        }
+        if y + 1 < height && tile_map[y + 1][x] == Tile::Garden && distances[y + 1][x] == std::usize::MAX {
+            distances[y + 1][x] = curr_tile_distance + 1;
+            bfs_queue.push_back((x, y + 1));
+        }
+        if y >= 1 && tile_map[y - 1][x] == Tile::Garden && distances[y - 1][x] == std::usize::MAX {
+            distances[y - 1][x] = curr_tile_distance + 1;
+            bfs_queue.push_back((x, y - 1));
+        }
+    }
+
+    println!("Max distance: {}", distances.iter().flatten().filter(|d| **d < std::usize::MAX).max().unwrap());
+    
+    // Count number of reachable tiles on each type of tile
+    // Corners are grouped into one, since it's symmetrical and they're equal in all 4 directions
+    let mut full_even_reach_count = 0;
+    let mut full_odd_reach_count = 0;
+    let mut even_corner_reach_count = 0;
+    let mut odd_corner_reach_count = 0;
+    for j in 0..height {
+        for i in 0..width {
+            if tile_map[j][i] == Tile::Garden {
+                if (i + j) % 2 == 0 {
+                    full_even_reach_count += 1;
+                    if distances[j][i] > (height / 2) {
+                        even_corner_reach_count += 1;
+                    }
+                }
+                else {
+                    full_odd_reach_count += 1;
+                    if distances[j][i] > (height / 2) {
+                        odd_corner_reach_count += 1;
+                    }
+                }
+            }
+        }
+    }
+    /*
+    for j in 0..height {
+        for i in 0..width {
+            if tile_map[j][i] == Tile::Garden {
+                if (i + j) % 2 == 0 {
+                    full_even_reach_count += 1;
+                    if i.abs_diff(width / 2) + j.abs_diff(height / 2) > (height / 2) {
+                        even_corner_reach_count += 1;
+                    }
+                }
+                else {
+                    full_odd_reach_count += 1;
+                    if i.abs_diff(width / 2) + j.abs_diff(height / 2) > (height / 2) {
+                        odd_corner_reach_count += 1;
+                    }
+                }
+            }
+        }
+    }
+    */
+
+    let num_steps = 26501365;
+    //let num_steps = 65 + 131 * 2;
+    let chunk_radius: i64 = ((num_steps - height / 2) / height).try_into().unwrap();
+    println!("chunk_radius: {chunk_radius}");
+    println!("full_even_reach_count: {full_even_reach_count}, full_odd_reach_count: {full_odd_reach_count}, even_corner_reach_count: {even_corner_reach_count}, odd_corner_reach_count: {odd_corner_reach_count}");
+    
+    let num_odd_chunks = (chunk_radius + 1) * (chunk_radius + 1);
+    let num_even_chunks = chunk_radius * chunk_radius;
+    let num_odd_corners = -1 * (chunk_radius + 1); // Odd corners are removed
+    let num_even_corners = chunk_radius;
+
+    let total = num_even_chunks * full_even_reach_count + num_odd_chunks * full_odd_reach_count + num_even_corners * even_corner_reach_count + num_odd_corners * odd_corner_reach_count;
+    println!("Total: {}", total);
+    //return num_even_chunks * full_even_reach_count + num_odd_chunks * full_odd_reach_count + num_even_corners * even_corner_reach_count + num_odd_corners * odd_corner_reach_count;
+    
+    /**/
+    let sim_map_width = 5 * width + 2;
+    let sim_map_height = 5 * height + 2;
+    let mut tile_map = vec![vec![Tile::Rock; sim_map_width]; sim_map_height]; // Simulate a 5x5 in order to get all odd/even and corner variations
+    for chunk_y in 0..5 {
+        for chunk_x in 0..5 {
+            for (i, ch) in tile_chars.char_indices() {
+                let tile_x = i % height;
+                let tile_y = i / height;
+                tile_map[tile_y + chunk_y * height + 1][tile_x + chunk_x * width + 1] = match ch {
+                    '#' => Tile::Rock,
+                    _ => Tile::Garden
+                }
+            }
+        }
+    }
+
+    // Generate odd/even and corner variations
+    //let num_steps = sim_map_width / 2 - 1;
+    let num_steps = 10;
+    println!("Simulating {} steps", num_steps);
+    let mut reachable = vec![vec![false; sim_map_height]; sim_map_width];
+    reachable[sim_map_height / 2][sim_map_width / 2] = true;
+    for _ in 0..num_steps {
+        let mut new_reachable = vec![vec![false; sim_map_height]; sim_map_width];
+        for j in 1..(sim_map_height - 1) {
+            for i in 1..(sim_map_width - 1) {
+                if tile_map[j][i] == Tile::Garden && (reachable[j - 1][i] || reachable[j + 1][i] || reachable[j][i - 1] || reachable[j][i + 1]) {
+                    new_reachable[j][i] = true;
+                }
+            }
+        }
+        reachable = new_reachable;
+    }
+    for j in 0..sim_map_height {
+        for i in 0..sim_map_width {
+            if reachable[j][i] {
+                print!("O");
+            }
+            else if tile_map[j][i] == Tile::Garden {
+                print!(".")
+            }
+            else {
+                print!("#");
+            }
+        }
+        println!("");
+    }
+    let full_odd_reach = count_reachable(&reachable, 2 * width + 1, 3 * width + 1, 2 * height + 1, 3 * height + 1);
+    let full_even_reach = count_reachable(&reachable, 3 * width + 1, 4 * width + 1, 2 * height + 1, 3 * height + 1);
+    let odd_corner_top_reach = count_reachable(&reachable, 2 * width + 1, 3 * width + 1, 1, height + 1);
+    let odd_corner_bottom_reach = count_reachable(&reachable, 2 * width + 1, 3 * width + 1, 4 * height + 1, 5 * height + 1);
+    let even_corner_tl_reach = count_reachable(&reachable, width + 1, 2 * width + 1, 1, height + 1);
+    let even_corner_tr_reach = count_reachable(&reachable, 3 * width + 1, 4 * width + 1, 1, height + 1);
+    let even_corner_bl_reach = count_reachable(&reachable, width + 1, 2 * width + 1, 4 * height + 1, 5 * height + 1);
+    let even_corner_br_reach = count_reachable(&reachable, 3 * width + 1, 4 * width + 1, 4 * height + 1, 5 * height + 1);
+    println!("full_odd_reach: {full_odd_reach}, full_even_reach: {full_even_reach}");
+    println!("odd_corner_top_reach: {odd_corner_top_reach}, odd_corner_bottom_reach: {odd_corner_bottom_reach}");
+    println!("even_corner_tl_reach: {even_corner_tl_reach}, even_corner_tr_reach: {even_corner_tr_reach}, even_corner_bl_reach: {even_corner_bl_reach}, even_corner_br_reach: {even_corner_br_reach}");
+    
+    // println!("{} reachable", count_reachable(&reachable, 0, sim_map_width, 0, sim_map_height));
+    /**/
+    return 0;
+}
+
+fn count_reachable(reachable: &Vec<Vec<bool>>, start_x: usize, end_x: usize, start_y: usize, end_y: usize) -> i32 {
+    let mut num_reachable = 0;
+    for j in start_y..end_y {
+        for i in start_x..end_x {
+            if reachable[j][i] {
+                num_reachable += 1;
+            }
+        }
+    }
+    return num_reachable;
+}
+
 #[derive(PartialEq, Eq, Hash)]
 struct AdjChunkStates {
     curr: u32,
@@ -116,7 +304,7 @@ impl ReachMapLookup {
     }
 }
 
-fn part2(lines: &Vec<String>) -> u64 {
+fn part2_failure(lines: &Vec<String>) -> u64 {
     let width = lines[0].len();
     let height = lines.len();
 
@@ -148,7 +336,8 @@ fn part2(lines: &Vec<String>) -> u64 {
     let mut reach_map_transitions: HashMap<AdjChunkStates, u32> = HashMap::new();
     let mut chunks: HashMap<(i32, i32), u32> = HashMap::new();
     let mut live_chunks: HashSet<(i32, i32)> = HashSet::new();
-    let mut num_filled_chunks = 0;
+    let mut num_filled_even_chunks = 0;
+    let mut num_filled_odd_chunks = 0;
 
     // Pre-generate maximally filled chunks
     let empty_chunk = vec![false; width * height];
@@ -171,10 +360,10 @@ fn part2(lines: &Vec<String>) -> u64 {
     }
 
     // Iterate through steps
-    let total_step_count = 5;
+    let total_step_count = 242;
     for step_num in 0..total_step_count {
         //println!("{:?}", chunks);
-        let filled_tile_state = ((step_num + start_index) % 2 + 1).try_into().unwrap();
+        //let filled_tile_state = ((step_num + start_index) % 2 + 1).try_into().unwrap();
 
         // Determine which chunks to change
         let mut changed_chunks: HashMap<(i32, i32), u32> = HashMap::new();
@@ -184,27 +373,42 @@ fn part2(lines: &Vec<String>) -> u64 {
             // Attempt to look up state change based on neighboring chunks, compute it otherwise
             // TODO: Make sure to check state for filled chunks correctly
             let curr_chunk_state = match chunks.get(&(chunk_x, chunk_y)).copied() {
-                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 0) => max_fill_even_id,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 1) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 0) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 1) => max_fill_even_id,
                 Some(id) => id,
                 None => unreachable!()
             };
             let north_chunk_state = match chunks.get(&(chunk_x, chunk_y - 1)).copied() {
-                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 0) => max_fill_even_id,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 1) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 0) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 1) => max_fill_even_id,
                 Some(id) => id,
                 None => empty_chunk_id
             };
             let south_chunk_state = match chunks.get(&(chunk_x, chunk_y + 1)).copied() {
-                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 0) => max_fill_even_id,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 1) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 0) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 1) => max_fill_even_id,
                 Some(id) => id,
                 None => empty_chunk_id
             };
             let east_chunk_state = match chunks.get(&(chunk_x + 1, chunk_y)).copied() {
-                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 0) => max_fill_even_id,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 1) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 0) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 1) => max_fill_even_id,
                 Some(id) => id,
                 None => empty_chunk_id
             };
             let west_chunk_state = match chunks.get(&(chunk_x - 1, chunk_y)).copied() {
-                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 0) => max_fill_even_id,
+                Some(id) if (id == max_fill_even_id) && (step_num % 2 == 1) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 0) => max_fill_odd_id,
+                Some(id) if (id == max_fill_odd_id) && (step_num % 2 == 1) => max_fill_even_id,
                 Some(id) => id,
                 None => empty_chunk_id
             };
@@ -227,9 +431,16 @@ fn part2(lines: &Vec<String>) -> u64 {
                     // When chunk enters a filled state, it is removed from live chunks
                     // Additionally, IDs are handled in a special manner, they are based on which state it is in on even-numbered steps
                     new_filled_chunks.insert((chunk_x, chunk_y));
-                    let filled_chunk_id = match (new_chunk_state == max_fill_even_id) ^ (step_num % 2 == 0) {
-                        true => max_fill_odd_id,
-                        false => max_fill_even_id
+                    let filled_chunk_id;
+                    match (new_chunk_state == max_fill_even_id) ^ (step_num % 2 == 1) {
+                        true => {
+                            filled_chunk_id = max_fill_odd_id;
+                            num_filled_odd_chunks += 1;
+                        },
+                        false => {
+                            filled_chunk_id = max_fill_even_id;
+                            num_filled_even_chunks += 1;
+                        }
                     };
                     changed_chunks.insert((chunk_x, chunk_y), filled_chunk_id);
                 }
@@ -246,36 +457,44 @@ fn part2(lines: &Vec<String>) -> u64 {
         }
         for coords in new_filled_chunks {
             live_chunks.remove(&coords);
-            num_filled_chunks += 1;
         }
         for (changed_chunk_coords, new_reach_id) in changed_chunks.iter() {
             chunks.insert(*changed_chunk_coords, *new_reach_id);
         }
 
         // Print test
-        println!("Step {}", step_num + 1);
-        print_chunks(&tile_map, &reach_map_lookup, &chunks, 3 - filled_tile_state);
+        //println!("Step {}", step_num + 1);
+        //print_chunks(&tile_map, &reach_map_lookup, &chunks, step_num + 1);
     }
 
     // Count total reachable tiles
     // Count number of each chunk ID, calculate reachable tiles in each different map
     println!("{} live chunks", live_chunks.len());
-    println!("{} filled chunks", num_filled_chunks);
+    println!("{} filled even chunks", num_filled_even_chunks);
+    println!("{} filled odd chunks", num_filled_odd_chunks);
     let mut end_chunk_id_count: HashMap<u32, u32> = HashMap::new();
     for coords in live_chunks {
         let chunk_id = chunks.get(&coords).copied().unwrap();
         end_chunk_id_count.insert(chunk_id, end_chunk_id_count.get(&chunk_id).copied().unwrap_or(0) + 1);
     }
     println!("{} empty chunks", end_chunk_id_count.get(&0).unwrap());
+    let mut chunk_counts_vec: Vec<(&u32, &u32)> = end_chunk_id_count.iter().collect();
+    chunk_counts_vec.sort();
+    println!("{:?}", chunk_counts_vec);
     
     let mut subtotal = 0;
     for (chunk_id, count) in end_chunk_id_count.iter() {
         let num_reachable_tiles: u64 = reach_map_lookup.get_reach_map(*chunk_id).unwrap().iter().filter(|r| **r).count().try_into().unwrap();
         subtotal += num_reachable_tiles * (*count as u64);
     }
-    let end_filled_tile_state = ((total_step_count + start_index) % 2 + 1).try_into().unwrap();
-    let filled_chunk_reachable_count: u32 = reach_map_lookup.get_reach_map(end_filled_tile_state).unwrap().iter().filter(|r| **r).count().try_into().unwrap();
-    subtotal += (filled_chunk_reachable_count * num_filled_chunks) as u64;
+    let filled_even_num_reachable: u64 = reach_map_lookup.get_reach_map(max_fill_even_id).unwrap().iter().filter(|r| **r).count().try_into().unwrap();
+    let filled_odd_num_reachable: u64 = reach_map_lookup.get_reach_map(max_fill_odd_id).unwrap().iter().filter(|r| **r).count().try_into().unwrap();
+    if total_step_count % 2 == 0 {
+        subtotal += num_filled_even_chunks * filled_even_num_reachable + num_filled_odd_chunks * filled_odd_num_reachable;
+    }
+    else {
+        subtotal += num_filled_even_chunks * filled_odd_num_reachable + num_filled_odd_chunks * filled_even_num_reachable;
+    }
     return subtotal;
 }
 
@@ -330,7 +549,7 @@ fn compute_chunk_state(tile_map: &Vec<Vec<Tile>>, reach_map_lookup: &mut ReachMa
     return new_chunk_id;
 }
 
-fn print_chunks(tile_map: &Vec<Vec<Tile>>, reach_map_lookup: &ReachMapLookup, chunks: &HashMap<(i32, i32), u32>, filled_tile_state: u32) {
+fn print_chunks(tile_map: &Vec<Vec<Tile>>, reach_map_lookup: &ReachMapLookup, chunks: &HashMap<(i32, i32), u32>, step_num: usize) {
     let width = reach_map_lookup.width;
     let height = reach_map_lookup.height;
     let min_chunk_x = chunks.keys().map(|(x, y)| *x).min().unwrap();
@@ -346,7 +565,14 @@ fn print_chunks(tile_map: &Vec<Vec<Tile>>, reach_map_lookup: &ReachMapLookup, ch
         let offset_chunk_y: usize = (*chunk_y - min_chunk_y).try_into().unwrap();
 
         let reach_map = match *chunk_id {
-            1 | 2 => reach_map_lookup.get_reach_map(filled_tile_state).unwrap(),
+            1 => match step_num % 2 {
+                0 => reach_map_lookup.get_reach_map(1).unwrap(),
+                _ => reach_map_lookup.get_reach_map(2).unwrap()
+            },
+            2 => match step_num % 2 {
+                0 => reach_map_lookup.get_reach_map(2).unwrap(),
+                _ => reach_map_lookup.get_reach_map(1).unwrap()
+            },
             id => reach_map_lookup.get_reach_map(id).unwrap()
         };
         for (i, reachable) in reach_map.iter().enumerate() {
