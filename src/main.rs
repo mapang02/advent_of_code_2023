575e14b37e27d66049e3c45d2f19ec1495274d7f
a <a>
2024-11-16 23:41:52 -0500

    Part 2 tentatively complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::{array, collections::{HashMap, HashSet}, hash::Hash, io};
 
 fn main() {
@@ -87,19 +88,21 @@ struct AdjChunkStates {
     west: u32
 }
 
-struct ReachMapIdLookup {
+struct ReachMapLookup {
+    width: usize,
+    height: usize,
     reach_map_ids: HashMap<Vec<bool>, u32>,
     reach_map_lookup: HashMap<u32, Vec<bool>>,
     num_reach_maps: u32
 }
-impl ReachMapIdLookup {
-    fn new() -> ReachMapIdLookup {
-        return ReachMapIdLookup { reach_map_ids: HashMap::new(), reach_map_lookup: HashMap::new(), num_reach_maps: 0 }
+impl ReachMapLookup {
+    fn new(width: usize, height: usize) -> ReachMapLookup {
+        return ReachMapLookup { width: width, height: height, reach_map_ids: HashMap::new(), reach_map_lookup: HashMap::new(), num_reach_maps: 0 }
     }
-    fn get_id(&mut self, reach_map: &Vec<bool>) -> Option<u32> {
+    fn get_id(&self, reach_map: &Vec<bool>) -> Option<u32> {
         return self.reach_map_ids.get(reach_map).copied();
     }
-    fn get_reach_map(&mut self, id: u32) -> Option<&Vec<bool>> {
+    fn get_reach_map(&self, id: u32) -> Option<&Vec<bool>> {
         return self.reach_map_lookup.get(&id);
     }
     fn insert(&mut self, reach_map: &Vec<bool>) -> Option<u32> {
@@ -113,7 +116,7 @@ impl ReachMapIdLookup {
     }
 }
 
-fn part2(lines: &Vec<String>) -> i32 {
+fn part2(lines: &Vec<String>) -> u64 {
     let width = lines[0].len();
     let height = lines.len();
 
@@ -141,7 +144,7 @@ fn part2(lines: &Vec<String>) -> i32 {
     // When entering a newly generated "chunk", it can be generated based on its neighbors
     // Chunks don't need to be tracked after all alternating tiles are filled
     // A chunk is "generated" when it is first added to the chunks map, and is "live" after being generated until reaching a filled state
-    let mut reach_map_ids = ReachMapIdLookup::new();
+    let mut reach_map_lookup = ReachMapLookup::new(width, height);
     let mut reach_map_transitions: HashMap<AdjChunkStates, u32> = HashMap::new();
     let mut chunks: HashMap<(i32, i32), u32> = HashMap::new();
     let mut live_chunks: HashSet<(i32, i32)> = HashSet::new();
@@ -152,14 +155,14 @@ fn part2(lines: &Vec<String>) -> i32 {
     let max_fill_even = (0..(width * height)).map(|i| i % 2 == 0 && tile_map[i % height][i / height] == Tile::Garden).collect();
     let max_fill_odd = (0..(width * height)).map(|i| i % 2 == 1 && tile_map[i % height][i / height] == Tile::Garden).collect();
 
-    reach_map_ids.insert(&empty_chunk);
-    reach_map_ids.insert(&max_fill_even);
-    reach_map_ids.insert(&max_fill_odd);
+    let empty_chunk_id = reach_map_lookup.insert(&empty_chunk).unwrap();
+    let max_fill_even_id = reach_map_lookup.insert(&max_fill_even).unwrap();
+    let max_fill_odd_id = reach_map_lookup.insert(&max_fill_odd).unwrap();
     
     // Generate starting chunk
     let mut start_reach = vec![false; width * height];
     start_reach[start_index] = true;
-    let start_reach_id = reach_map_ids.insert(&start_reach).unwrap();
+    let start_reach_id = reach_map_lookup.insert(&start_reach).unwrap();
     chunks.insert((0, 0), start_reach_id);
     live_chunks.insert((0, 0));
     for adj_chunk_coords in [(-1, 0), (1, 0), (0, -1), (0, 1)] { // Make chunks adjacent to start chunk live
@@ -170,19 +173,43 @@ fn part2(lines: &Vec<String>) -> i32 {
     // Iterate through steps
     let total_step_count = 6;
     for step_num in 0..total_step_count {
+        let filled_tile_state = ((step_num + start_index) % 2 + 1).try_into().unwrap();
+
         // Determine which chunks to change
         let mut changed_chunks: HashMap<(i32, i32), u32> = HashMap::new();
         let mut new_live_chunks: HashSet<(i32, i32)> = HashSet::new();
+        let mut new_filled_chunks: HashSet<(i32, i32)> = HashSet::new();
         for (chunk_x, chunk_y) in live_chunks.iter().copied() {
             // Attempt to look up state change based on neighboring chunks, compute it otherwise
-            let curr_chunk_state = chunks.get(&(chunk_x, chunk_y)).copied().unwrap();
-            let north_chunk_state = chunks.get(&(chunk_x, chunk_y + 1)).copied().unwrap_or(0);
-            let south_chunk_state = chunks.get(&(chunk_x, chunk_y - 1)).copied().unwrap_or(0);
-            let east_chunk_state = chunks.get(&(chunk_x + 1, chunk_y)).copied().unwrap_or(0);
-            let west_chunk_state = chunks.get(&(chunk_x - 1, chunk_y)).copied().unwrap_or(0);
+            // TODO: Make sure to check state for filled chunks correctly
+            let curr_chunk_state = match chunks.get(&(chunk_x, chunk_y)).copied() {
+                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) => id,
+                None => unreachable!()
+            };
+            let north_chunk_state = match chunks.get(&(chunk_x, chunk_y + 1)).copied() {
+                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) => id,
+                None => empty_chunk_id
+            };
+            let south_chunk_state = match chunks.get(&(chunk_x, chunk_y - 1)).copied() {
+                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) => id,
+                None => empty_chunk_id
+            };
+            let east_chunk_state = match chunks.get(&(chunk_x + 1, chunk_y)).copied() {
+                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) => id,
+                None => empty_chunk_id
+            };
+            let west_chunk_state = match chunks.get(&(chunk_x - 1, chunk_y)).copied() {
+                Some(id) if (id == max_fill_even_id || id == max_fill_odd_id) => filled_tile_state,
+                Some(id) => id,
+                None => empty_chunk_id
+            };
 
             let adj_chunk_states = AdjChunkStates { curr: curr_chunk_state, north: north_chunk_state, south: south_chunk_state, east: east_chunk_state, west: west_chunk_state };
-            let new_chunk_state = reach_map_transitions.get(&adj_chunk_states).copied().unwrap_or_else(|| compute_chunk_state(&reach_map_ids, adj_chunk_states));
+            let new_chunk_state = reach_map_transitions.get(&adj_chunk_states).copied().unwrap_or_else(|| compute_chunk_state(&tile_map, &mut reach_map_lookup, adj_chunk_states));
             if new_chunk_state != curr_chunk_state {
                 changed_chunks.insert((chunk_x, chunk_y), new_chunk_state);
 
@@ -196,22 +223,93 @@ fn part2(lines: &Vec<String>) -> i32 {
                         }
                     }
                 }
+
+                // When chunk enters a filled state, remove it from live chunks
+                if (curr_chunk_state == max_fill_even_id) || (curr_chunk_state == max_fill_odd_id) {
+                    new_filled_chunks.insert((chunk_x, chunk_y));
+                }
             }
         }
 
-        // Add newly live chunks and carry out state changes
+        // Update set of live chunks and carry out state changes
         for coords in new_live_chunks {
             chunks.insert(coords, 0);
             live_chunks.insert(coords);
+        }
+        for coords in new_filled_chunks {
+            live_chunks.remove(&coords);
+            num_filled_chunks += 1;
         }
         for (changed_chunk_coords, new_reach_id) in changed_chunks.iter() {
             chunks.insert(*changed_chunk_coords, *new_reach_id);
         }
     }
 
-    return 0;
+    // Count total reachable tiles
+    // Count number of each chunk ID, calculate reachable tiles in each different map
+    let mut end_chunk_id_count: HashMap<u32, u32> = HashMap::new();
+    for coords in live_chunks {
+        let chunk_id = chunks.get(&coords).copied().unwrap();
+        end_chunk_id_count.insert(chunk_id, end_chunk_id_count.get(&chunk_id).copied().unwrap_or(0) + 1);
+    }
+    let end_filled_tile_state = ((total_step_count + start_index) % 2).try_into().unwrap();
+    let filled_chunk_reachable_count = reach_map_lookup.get_reach_map(end_filled_tile_state).unwrap().iter().filter(|r| **r).count().try_into().unwrap();
+    end_chunk_id_count.insert(end_filled_tile_state, filled_chunk_reachable_count);
+    
+    let mut subtotal = 0;
+    for (chunk_id, count) in end_chunk_id_count.iter() {
+        let num_reachable_tiles: u64 = reach_map_lookup.get_reach_map(*chunk_id).unwrap().iter().filter(|r| **r).count().try_into().unwrap();
+        subtotal += num_reachable_tiles * (*count as u64);
+    }
+    return subtotal;
 }
 
-fn compute_chunk_state(reach_map_ids: &ReachMapIdLookup, AdjChunkStates { curr, north, south, east, west }: AdjChunkStates) -> u32 {
-    let curr_chunk_map = reach_map_ids;
+fn compute_chunk_state(tile_map: &Vec<Vec<Tile>>, reach_map_lookup: &mut ReachMapLookup, AdjChunkStates { curr, north, south, east, west }: AdjChunkStates) -> u32 {
+    let height = reach_map_lookup.height;
+    let width = reach_map_lookup.width;
+    let num_chunk_tiles = width * height;
+
+    // Build simulation
+    let mut sim_tile_map = vec![vec![Tile::Rock; 3 * width + 2]; 3 * height + 2];
+    for (x_offset, y_offset) in [(1, 1), (0, 1), (2, 1), (1, 0), (1, 2)] {
+        for i in 0..height {
+            for j in 0..width {
+                sim_tile_map[height * x_offset + i + 1][width * y_offset + j + 1] = tile_map[i][j];
+            }
+        }
+    }
+
+    let curr_chunk_map = reach_map_lookup.get_reach_map(curr).unwrap();
+    let north_chunk_map = reach_map_lookup.get_reach_map(north).unwrap();
+    let south_chunk_map = reach_map_lookup.get_reach_map(south).unwrap();
+    let east_chunk_map = reach_map_lookup.get_reach_map(east).unwrap();
+    let west_chunk_map = reach_map_lookup.get_reach_map(west).unwrap();
+
+    let mut sim_reach_map = vec![vec![false; 3 * width]; 3 * height];
+    for (x_offset, y_offset, reach_map) in [(1, 1, curr_chunk_map), (1, 0, north_chunk_map), (1, 2, south_chunk_map), (0, 1, west_chunk_map), (2, 1, east_chunk_map)] {
+        for (i, tl) in reach_map.iter().enumerate() {
+            let tile_x = x_offset * width + 1 + (i % height);
+            let tile_y = y_offset * height + 1 + (i / height);
+            sim_reach_map[tile_x][tile_y] = *tl;
+        }
+    }
+
+    // Compute next step
+    let mut sim_reach_map_next = vec![vec![false; 3 * width]; 3 * height];
+    for j in 1..(3 * height + 1) {
+        for i in 1..(3 * width + 1) {
+            if sim_tile_map[i][j] == Tile::Garden && (sim_reach_map[i - 1][j] || sim_reach_map[i + 1][j] || sim_reach_map[i][j - 1] || sim_reach_map[i][j + 1]) {
+                sim_reach_map_next[i][j] = true;
+            }
+        }
+    }
+
+    // Check ID of new chunk state, create new ID if state has not been recorded yet
+    let new_reach_map = (0..num_chunk_tiles).into_iter().map(|i| sim_reach_map_next[width + 1 + (i % height)][height + 1 + (i / height)]).collect();
+    let new_chunk_id;
+    match reach_map_lookup.get_id(&new_reach_map) {
+        Some(id) => { new_chunk_id = id },
+        None => { new_chunk_id = reach_map_lookup.insert(&new_reach_map).unwrap() }
+    }
+    return new_chunk_id;
 }
