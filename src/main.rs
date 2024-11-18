64c648e5b03ff10c9e38bb1bc84c430d42ad9349
a <a>
2024-11-18 01:24:48 -0500

    WIP on counting with BFS, about to remove bad code

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ fn part1(lines: &Vec<String>) -> i32 {
     return reachable.into_iter().flatten().filter(|r| *r).count() as i32;
 }
 
-fn part2(lines: &Vec<String>) -> i64 {
+fn part2(lines: &Vec<String>) -> u64 {
     let width = lines[0].len();
     let height = lines.len();
 
@@ -177,20 +177,83 @@ fn part2(lines: &Vec<String>) -> i64 {
 
     let num_steps = 26501365;
     //let num_steps = 65 + 131 * 2;
-    let chunk_radius: i64 = ((num_steps - height / 2) / height).try_into().unwrap();
+    let chunk_radius: u64 = ((num_steps - height / 2) / height).try_into().unwrap();
     println!("chunk_radius: {chunk_radius}");
     println!("full_even_reach_count: {full_even_reach_count}, full_odd_reach_count: {full_odd_reach_count}, even_corner_reach_count: {even_corner_reach_count}, odd_corner_reach_count: {odd_corner_reach_count}");
     
     let num_odd_chunks = (chunk_radius + 1) * (chunk_radius + 1);
     let num_even_chunks = chunk_radius * chunk_radius;
-    let num_odd_corners = -1 * (chunk_radius + 1); // Odd corners are removed
+    let num_odd_corners = (chunk_radius + 1); // Odd corners are removed
     let num_even_corners = chunk_radius;
 
     let total = num_even_chunks * full_even_reach_count + num_odd_chunks * full_odd_reach_count + num_even_corners * even_corner_reach_count + num_odd_corners * odd_corner_reach_count;
     println!("Total: {}", total);
     //return num_even_chunks * full_even_reach_count + num_odd_chunks * full_odd_reach_count + num_even_corners * even_corner_reach_count + num_odd_corners * odd_corner_reach_count;
     
-    /**/
+    let sim_map_width = 5 * width;
+    let sim_map_height = 5 * height;
+    let mut sim_tile_map = vec![vec![Tile::Rock; sim_map_width]; sim_map_height]; // Simulate a 5x5 in order to get all odd/even and corner variations
+    for chunk_y in 0..5 {
+        for chunk_x in 0..5 {
+            for (i, ch) in tile_chars.char_indices() {
+                let tile_x = i % height;
+                let tile_y = i / height;
+                sim_tile_map[tile_y + chunk_y * height][tile_x + chunk_x * width] = match ch {
+                    '#' => Tile::Rock,
+                    _ => Tile::Garden
+                }
+            }
+        }
+    }
+
+    let mut sim_distances = vec![vec![std::usize::MAX; sim_map_width]; sim_map_height];
+    let mut sim_bfs_queue = VecDeque::new();
+
+    sim_distances[sim_map_height / 2][sim_map_width / 2] = 0;
+    sim_bfs_queue.push_back((sim_map_height / 2, sim_map_width / 2));
+    while !sim_bfs_queue.is_empty() {
+        let (x, y) = sim_bfs_queue.pop_front().unwrap();
+        let curr_tile_distance = sim_distances[y][x];
+        if x + 1 < sim_map_width && sim_tile_map[y][x + 1] == Tile::Garden && sim_distances[y][x + 1] == std::usize::MAX {
+            sim_distances[y][x + 1] = curr_tile_distance + 1;
+            sim_bfs_queue.push_back((x + 1, y));
+        }
+        if x >= 1 && sim_tile_map[y][x - 1] == Tile::Garden && sim_distances[y][x - 1] == std::usize::MAX {
+            sim_distances[y][x - 1] = curr_tile_distance + 1;
+            sim_bfs_queue.push_back((x - 1, y));
+        }
+        if y + 1 < sim_map_height && sim_tile_map[y + 1][x] == Tile::Garden && sim_distances[y + 1][x] == std::usize::MAX {
+            sim_distances[y + 1][x] = curr_tile_distance + 1;
+            sim_bfs_queue.push_back((x, y + 1));
+        }
+        if y >= 1 && sim_tile_map[y - 1][x] == Tile::Garden && sim_distances[y - 1][x] == std::usize::MAX {
+            sim_distances[y - 1][x] = curr_tile_distance + 1;
+            sim_bfs_queue.push_back((x, y - 1));
+        }
+    }
+
+    let num_sim_steps = 2 * width + width / 2;
+    let reachable = sim_distances.iter().map(|row| row.iter().map(|d| *d <= num_sim_steps && (d + num_sim_steps) % 2 == 0).collect()).collect();
+
+    let full_odd_reach = count_reachable(&reachable, 2 * width, 3 * width, 2 * height, 3 * height);
+    let full_even_reach = count_reachable(&reachable, 3 * width, 4 * width, 2 * height, 3 * height);
+    let odd_corner_top_reach = count_reachable(&reachable, 2 * width, 3 * width, 0, height);
+    let odd_corner_bottom_reach = count_reachable(&reachable, 2 * width, 3 * width, 4 * height, 5 * height);
+    let even_corner_tl_reach = count_reachable(&reachable, width, 2 * width, 0, height);
+    let even_corner_tr_reach = count_reachable(&reachable, 3 * width, 4 * width, 0, height);
+    let even_corner_bl_reach = count_reachable(&reachable, width, 2 * width, 4 * height, 5 * height);
+    let even_corner_br_reach = count_reachable(&reachable, 3 * width, 4 * width, 4 * height, 5 * height);
+    // let total_reachable = count_reachable(&reachable, 0, sim_map_width, 0, sim_map_height);
+    println!("full_odd_reach: {full_odd_reach}, full_even_reach: {full_even_reach}");
+    println!("odd_corner_top_reach: {odd_corner_top_reach}, odd_corner_bottom_reach: {odd_corner_bottom_reach}");
+    println!("even_corner_tl_reach: {even_corner_tl_reach}, even_corner_tr_reach: {even_corner_tr_reach}, even_corner_bl_reach: {even_corner_bl_reach}, even_corner_br_reach: {even_corner_br_reach}");
+    
+    let even_corner_reach_count = even_corner_tl_reach + even_corner_tr_reach + even_corner_bl_reach + even_corner_tr_reach;
+    let odd_corner_reach_count = 2 * full_odd_reach - (odd_corner_top_reach + odd_corner_bottom_reach);
+    let total = num_even_chunks * full_even_reach + num_odd_chunks * full_odd_reach + num_even_corners * even_corner_reach_count - num_odd_corners * odd_corner_reach_count;
+    println!("total: {total}");
+
+    /*
     let sim_map_width = 5 * width + 2;
     let sim_map_height = 5 * height + 2;
     let mut tile_map = vec![vec![Tile::Rock; sim_map_width]; sim_map_height]; // Simulate a 5x5 in order to get all odd/even and corner variations
@@ -210,6 +273,7 @@ fn part2(lines: &Vec<String>) -> i64 {
     // Generate odd/even and corner variations
     //let num_steps = sim_map_width / 2 - 1;
     let num_steps = 10;
+
     println!("Simulating {} steps", num_steps);
     let mut reachable = vec![vec![false; sim_map_height]; sim_map_width];
     reachable[sim_map_height / 2][sim_map_width / 2] = true;
@@ -238,6 +302,7 @@ fn part2(lines: &Vec<String>) -> i64 {
         }
         println!("");
     }
+
     let full_odd_reach = count_reachable(&reachable, 2 * width + 1, 3 * width + 1, 2 * height + 1, 3 * height + 1);
     let full_even_reach = count_reachable(&reachable, 3 * width + 1, 4 * width + 1, 2 * height + 1, 3 * height + 1);
     let odd_corner_top_reach = count_reachable(&reachable, 2 * width + 1, 3 * width + 1, 1, height + 1);
@@ -249,13 +314,14 @@ fn part2(lines: &Vec<String>) -> i64 {
     println!("full_odd_reach: {full_odd_reach}, full_even_reach: {full_even_reach}");
     println!("odd_corner_top_reach: {odd_corner_top_reach}, odd_corner_bottom_reach: {odd_corner_bottom_reach}");
     println!("even_corner_tl_reach: {even_corner_tl_reach}, even_corner_tr_reach: {even_corner_tr_reach}, even_corner_bl_reach: {even_corner_bl_reach}, even_corner_br_reach: {even_corner_br_reach}");
+    */
     
     // println!("{} reachable", count_reachable(&reachable, 0, sim_map_width, 0, sim_map_height));
     /**/
     return 0;
 }
 
-fn count_reachable(reachable: &Vec<Vec<bool>>, start_x: usize, end_x: usize, start_y: usize, end_y: usize) -> i32 {
+fn count_reachable(reachable: &Vec<Vec<bool>>, start_x: usize, end_x: usize, start_y: usize, end_y: usize) -> u64 {
     let mut num_reachable = 0;
     for j in start_y..end_y {
         for i in start_x..end_x {
